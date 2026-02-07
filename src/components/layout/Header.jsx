f28df6bc76bc485a10a6b5c9d1c1c994f0f9aa27
Saravasha <33827803+Saravasha@users.noreply.github.com>
2026-02-07 03:13:18 +0100

    fixed tokens

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         className="m-4 p-4 text-shadow-2xl w-full text-center !text-6xl sm:!text-[8rem] text-wrap  hover:animate-pulse drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
         style={headerText}
       >
-        
+        __PROJECT_NAME__
       </h1>
     </div>
   );
